0c3a6050f69fccbedd80c076981b85c90944040e
gekas145 <56126336+gekas145@users.noreply.github.com>
2021-07-05 17:36:39 +0200

    update main

--- pacman_py/main.py
+++ pacman_py/main.py
@@ -1,5 +1,16 @@
+import numpy as np
+import pygame
+
 def main():
-    print("Hello Github")
+    (width, height) = (600, 600)
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.update()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 
 if __name__ == "__main__":
