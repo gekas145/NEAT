29bfea7ba7900397ba36dba06da17f9452fbd754
gekas145 <56126336+gekas145@users.noreply.github.com>
2021-10-23 23:30:31 +0200

    probably finish dino game

--- trex/trex.py
+++ trex/trex.py
@@ -31,11 +31,18 @@ def draw_next_game_step(draw_hitboxes=True):
         pygame.draw.rect(screen, black, trex_current_hitboxes[0], width=2)
         pygame.draw.rect(screen, black, trex_current_hitboxes[1], width=2)
         pygame.draw.rect(screen, black, current_hindrance, width=2)
+        if not is_cactus:
+            pygame.draw.rect(screen, black, pterodactylus_current_body_hitbox, width=2)
     clock.tick(100)
 
 
-# def collision_happened():
-#     return trex_current_hitboxes.colliderect(current_hindrance)
+def collision_happened():
+    for hitbox in trex_current_hitboxes:
+        if hitbox.colliderect(current_hindrance):
+            return True
+        if not is_cactus and hitbox.colliderect(pterodactylus_current_body_hitbox):
+            return True
+    return False
 
 
 space = pymunk.Space()
@@ -44,6 +51,8 @@ space.damping = 1
 
 gray = (127, 127, 127)
 black = (0, 0, 0)
+red = (255, 0, 0)
+
 field_width, field_height = 600, 600
 
 trex = pymunk.Body(1, 1, pymunk.Body.DYNAMIC)
@@ -89,9 +98,13 @@ cactuses_hitboxes = [pygame.Rect(field_width, 520, 120, 90),
 pterodactylus = [pygame.transform.scale(pygame.image.load('images/pterodactylus1.png'), (100, 60)),
                  pygame.transform.scale(pygame.image.load('images/pterodactylus2.png'), (100, 60))]
 
-pterodactylus_hitboxes = [pygame.Rect(field_width, 460, 100, 50),
-                          pygame.Rect(field_width, 480, 100, 50),
-                          pygame.Rect(field_width, 540, 100, 60)]
+pterodactylus_hitboxes = [pygame.Rect(field_width, 440, 30, 30),
+                          pygame.Rect(field_width, 540, 30, 30),
+                          pygame.Rect(field_width, 480, 30, 30)]
+pterodactylus_body_hitboxes = [pygame.Rect((field_width + 30, 455), (60, 30)),
+                               pygame.Rect((field_width + 30, 555), (60, 30)),
+                               pygame.Rect((field_width + 30, 495), (60, 30))]
+pterodactylus_current_body_hitbox = pterodactylus_body_hitboxes[0].copy()
 
 index = randint(0, len(cactuses) - 1)
 current_hindrance = cactuses_hitboxes[index].copy()
@@ -141,25 +154,38 @@ while True:
         trex_down = False
         calculate_trex_hitbox_pos()
 
-    draw_next_game_step()
+    draw_next_game_step(False)
     space.step(1 / 50)
 
-    if current_hindrance.x + current_hindrance.width <= 0:
-        if uniform(0, 1) < 0.5:
+    if is_cactus:
+        width_to_add = current_hindrance.width
+    else:
+        width_to_add = pterodactylus_current_body_hitbox.width
+
+    if current_hindrance.x + width_to_add <= 0:
+        if uniform(0, 1) < 0.45:
             is_cactus = True
             new_hindrance_index = randint(0, len(cactuses) - 1)
             current_hindrance = cactuses_hitboxes[new_hindrance_index].copy()
             current_hindrance_image = cactuses[new_hindrance_index].copy()
         else:
             is_cactus = False
-            current_hindrance = pterodactylus_hitboxes[randint(0, len(pterodactylus_hitboxes) - 1)].copy()
+            if uniform(0, 1) < 0.55:
+                current_hindrance = pterodactylus_hitboxes[-1].copy()
+                pterodactylus_current_body_hitbox = pterodactylus_body_hitboxes[-1].copy()
+            else:
+                new_hindrance_index = randint(0, len(pterodactylus_hitboxes) - 2)
+                current_hindrance = pterodactylus_hitboxes[new_hindrance_index].copy()
+                pterodactylus_current_body_hitbox =pterodactylus_body_hitboxes[new_hindrance_index].copy()
             current_hindrance_image = pterodactylus[0].copy()
     else:
         current_hindrance.x -= hindrance_speed
+        if not is_cactus:
+            pterodactylus_current_body_hitbox.x -= hindrance_speed
         # pygame.draw.rect(screen, rect=current_cactus, color=[0, 0, 0])
         screen.blit(current_hindrance_image, (current_hindrance.x, current_hindrance.y))
 
-    # if collision_happened():
-    #     quit_game()
-
     pygame.display.update()
+
+    if collision_happened():
+        quit_game()
