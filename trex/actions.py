4cd73438b898702aa78666150193a49ca4dd7333
gekas145 <56126336+gekas145@users.noreply.github.com>
2022-02-12 21:24:31 +0100

    clean up

--- trex/actions.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class Actions(Enum):
-    UP = 0
-    DOWN = 1
-    RELEASE_DOWN = 3
--- trex/simple_agent.py
+++
@@ -1,7 +0,0 @@
-from random import uniform
-from actions import Actions
-
-
-class SimpleAgent:
-    def get_decision(self):
-        return Actions.UP
